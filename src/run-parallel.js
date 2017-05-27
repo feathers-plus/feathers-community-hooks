b32f834eed83c0b438350190f70e2c2cc2de1baf
John Szwaronek <johnsz9999@gmail.com>
2017-05-27 15:35:13 -0400

    Added runParallel to run hooks in parallel to Feathers method call.

--- src/run-parallel.js
+++ src/run-parallel.js
@@ -2,20 +2,20 @@
 /*
  runParallel(func, cloneDepth)
  Run a hook function in parallel to the Feathers method call.
- 
+
  Parameters:
  - `func`       The function to run. Its signature is `func(contextClone)`
  - `cloneDepth` Depth to which the context is to be cloned. Default is 0 = do not clone.
                 A depth of 5 would clone context.result.data.[].item
- 
+
  Notes:
  - Feathers will keep on mutating `contextClone` if that has not been cloned.
- 
+
  Example:
  app.service('users').after({
    create: runParallel(sendSignupEmail())
  });
- 
+
  Author: Original version by bedeoverend
  */
 
@@ -26,10 +26,10 @@ export default function (func, cloneDepth = 0) {
   if (typeof func !== 'function') {
     throw new errors.BadRequest('Function not provided. (delay');
   }
-  
+
   return function (context) {
-    const copy = cloneDepth ? clone(context, true, cloneDepth) : context
-    
-    setTimeout(() => func.call(this, copy))
+    const copy = cloneDepth ? clone(context, true, cloneDepth) : context;
+
+    setTimeout(() => func.call(this, copy));
   };
-};
+}
